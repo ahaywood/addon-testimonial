f6dce898d965e0d961fe0ff0192886afc6743016
justinvdm <justinvderm@gmail.com>
2025-07-17 11:42:27 +0200

    progress

--- src/app/addons/testimonials/durableObject.ts
+++ src/app/addons/testimonials/durableObject.ts
@@ -2,7 +2,6 @@ import { type TestimonialsDatabase } from "./db/db";
 import { migrations as testimonialsMigrations } from "./db/migrations";
 import { SqliteDurableObject } from "rwsdk/db";
 
-console.log("################## testimonials durable object");
 export class TestimonialsDurableObject extends SqliteDurableObject<TestimonialsDatabase> {
   migrations = testimonialsMigrations;
 }
